437b2c649903cc985a3ac3824a0a9dd799a6b359
RexP10r <jlimonadnui@gmail.com>
2026-04-11 18:37:15 +0300

    solved p236

---
+++ src/bin/p236.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+use std::cell::{Ref, RefCell};
+use std::rc::Rc;
+struct Solution;
+impl Solution {
+    pub fn lowest_common_ancestor(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if root.is_none() || root == p || root == q {
+            return root;
+        };
+        let (left_child, right_child) = {
+            let node = root.as_ref().unwrap().borrow();
+            (node.left.clone(), node.right.clone())
+        };
+        let left = Self::lowest_common_ancestor(left_child, p.clone(), q.clone());
+        if left.is_some() && left != p && left != q {
+            return left;
+        };
+        let right = Self::lowest_common_ancestor(right_child, p.clone(), q.clone());
+        if left.is_some() && right.is_some() {
+            return root;
+        };
+        left.or(right)
+    }
+}
+fn main() {}
